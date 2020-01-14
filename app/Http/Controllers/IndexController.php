317a9eba4a5172c013a79a307b4473817083cb78
Magomedov Michael <magomedov.michael@gmail.com>
2020-01-14 13:30:46 +0300

    Update IndexController.php

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,15 +1,11 @@
 <?php
-
 declare(strict_types=1);
-
 namespace App\Http\Controllers;
-
 use App\Services\GoogleNeuralService;
 use App\Services\RegionByPhoneNumberDetector;
 use App\Services\VoiceRequestResolver;
 use Illuminate\Http\Request;
 use Psr\Log\LoggerInterface;
-
 final class IndexController extends Controller
 {
     public function index(
@@ -18,12 +14,11 @@ final class IndexController extends Controller
     ) {
         return view('index');
     }
-
-    public function audio(
+    public function vox(
         Request $request,
         VoiceRequestResolver $requestResolver,
         LoggerInterface $logger
     ): void {
-        $requestResolver->execute('+79046660000', $request->file('audio'));
+        $requestResolver->execute($request->get('phone'), $request->get('media'));
     }
 }
