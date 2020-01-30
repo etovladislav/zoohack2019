d6bcedd617fea294feae9fb5c866f8bed60e3ad3
Magomedov Michael <magomedov.michael@gmail.com>
2020-01-30 09:37:36 +0300

    Update FileUtil.php

--- app/FileUtil.php
+++ app/FileUtil.php
@@ -7,7 +7,7 @@ final class FileUtil
 {
     public static function saveFromUrl(string $url): string
     {
-        $file     = file_get_contents($url.'&api_key=749c3091-f103-4871-9229-a8b05425cb88');
+        $file     = file_get_contents(html_entity_decode($url) . '&api_key=749c3091-f103-4871-9229-a8b05425cb88');
         $fileName = Uuid::uuid4()->toString() . '.flac';
         Storage::disk('public')->put($fileName, $file);
         return $fileName;
